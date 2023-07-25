d30c4f952b190d3a971d114189f37ac517505b7b
Ethan Muller <hi@emu.media>
2023-07-25 11:36:44 -0400

    save circles

--- src/DrawingPad.tsx
+++ src/DrawingPad.tsx
@@ -1,23 +1,36 @@
 import { useEffect, useState, useRef } from "react"
 
 export default function(props) {
-    const [ count, setCount ] = useState(0)
+    const [ circles, setCircles ] = useState<any[]>(JSON.parse(localStorage.getItem('circles') || '{}'))
     const cnv = useRef<HTMLCanvasElement>(null)
 
-    function handlePointerDown() {
-        setCount((c) => c + 1)
-        console.log(count)
+    function clear() {
+      console.log('clearing')
+      setCircles([])
+    }
+
+    function handlePointerDown(event) {
+        const { top, left } = event.target.getBoundingClientRect()
+        const localX = event.clientX - left;
+        const localY = event.clientY - top;
+        setCircles([ ...circles, [localX, localY]])
     }
 
     useEffect(() => {
-        const ctx = cnv.current?.getContext('2d')
-        ctx.fillStyle = '#000000'
-        for (let i = 0; i < count; i++) {
-            ctx.beginPath()
-            ctx.arc(10*count, 100, 4, 0, 2*Math.PI)
-            ctx.fill()
-        }
-    }, [count])
+      localStorage.setItem('circles', JSON.stringify(circles))
+    }, [circles])
+
+    useEffect(() => {
+      const ctx = cnv.current?.getContext('2d');
+      ctx.clearRect(0, 0, cnv.current.width, cnv.current.height);
+
+      ctx.fillStyle = '#000000';
+      for (let i = 0; i < circles.length; i++) {
+        ctx.beginPath();
+        ctx.arc(circles[i][0], circles[i][1], 4, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    }, [circles]);
 
     useEffect(() => {
         cnv.current?.addEventListener('pointerdown', handlePointerDown)
@@ -30,6 +43,7 @@ export default function(props) {
     return (
         <>
         <canvas ref={cnv} {...props}></canvas>
+        <div><button onClick={clear}>clear</button></div>
         </>
     )
 }
