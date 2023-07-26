7b4e8886710961d74afd8e8aac830dd74fdfdbcd
Ethan Muller <hi@emu.media>
2023-07-26 10:36:16 -0400

    reformat file

--- src/DrawingPad.tsx
+++ src/DrawingPad.tsx
@@ -1,66 +1,76 @@
 import { useEffect, useState, useRef } from "react"
 
 export default function(props) {
-    const [ circles, setCircles ] = useState<any[]>(JSON.parse(localStorage.getItem('circles') || '[]'))
-    const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [ circles, setCircles ] = useState<any[]>(JSON.parse(localStorage.getItem('circles') || '[]'))
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
-    function clear() {
-      setCircles([])
+  function clear() {
+    setCircles([])
+  }
+
+  function handlePointerDown(event) {
+    const { top, left } = event.target.getBoundingClientRect()
+    const localX = event.clientX - left;
+    const localY = event.clientY - top;
+    setCircles([ ...circles, [localX, localY]])
+  }
+
+  function handlePointerMove(event) {
+  }
+
+  function handlePointerUp(event) {
+  }
+
+  useEffect(() => {
+    localStorage.setItem('circles', JSON.stringify(circles))
+  }, [circles])
+
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas?.getContext('2d');
+    const ratio = window.devicePixelRatio || 1;
+    if (canvas) {
+      canvas.width = props.width * ratio
+      canvas.height = props.height * ratio
+      canvas.style.width = props.width + 'px'
+      canvas.style.height = props.height + 'px'
     }
+    ctx?.scale(ratio, ratio)
+  }, [])
 
-    function handlePointerDown(event) {
-        const { top, left } = event.target.getBoundingClientRect()
-        const localX = event.clientX - left;
-        const localY = event.clientY - top;
-        setCircles([ ...circles, [localX, localY]])
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const ctx = canvas?.getContext('2d');
+    if (canvas && ctx) {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = '#000000';
+      for (let i = 0; i < circles.length; i++) {
+        ctx.beginPath();
+        ctx.arc(circles[i][0], circles[i][1], 2, 0, 2 * Math.PI);
+        ctx.fill();
+      }
     }
+  }, [circles]);
 
-    useEffect(() => {
-      localStorage.setItem('circles', JSON.stringify(circles))
-    }, [circles])
+  useEffect(() => {
+    const canvas = canvasRef.current
+    if (canvas) {
+      canvas.addEventListener('pointerdown', handlePointerDown)
+      canvas.addEventListener('pointermove', handlePointerMove)
+      canvas.addEventListener('pointerup', handlePointerUp)
 
-    useEffect(() => {
-      const canvas = canvasRef.current
-      const ctx = canvas?.getContext('2d');
-      const ratio = window.devicePixelRatio || 1;
-      if (canvas) {
-        canvas.width = props.width * ratio
-        canvas.height = props.height * ratio
-        canvas.style.width = props.width + 'px'
-        canvas.style.height = props.height + 'px'
+      return () => {
+        canvas.removeEventListener('pointerdown', handlePointerDown)
+        canvas.removeEventListener('pointermove', handlePointerMove)
+        canvas.removeEventListener('pointerup', handlePointerUp)
       }
-      ctx?.scale(ratio, ratio)
-    }, [])
+    }
+  })
 
-    useEffect(() => {
-      const canvas = canvasRef.current
-      const ctx = canvas?.getContext('2d');
-      if (canvas && ctx) {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = '#000000';
-        for (let i = 0; i < circles.length; i++) {
-          ctx.beginPath();
-          ctx.arc(circles[i][0], circles[i][1], 2, 0, 2 * Math.PI);
-          ctx.fill();
-        }
-      }
-    }, [circles]);
-
-    useEffect(() => {
-        const canvas = canvasRef.current
-        if (canvas) {
-          canvas.addEventListener('pointerdown', handlePointerDown)
-
-          return () => {
-            canvas.removeEventListener('pointerdown', handlePointerDown)
-          }
-        }
-    })
-
-    return (
-        <>
-        <canvas style={{touchAction: 'none'}} ref={canvasRef} {...props}></canvas>
-        <div><button onClick={clear}>clear</button></div>
-        </>
-    )
+  return (
+    <>
+    <canvas style={{touchAction: 'none'}} ref={canvasRef} {...props}></canvas>
+    <div><button onClick={clear}>clear</button></div>
+    </>
+  )
 }
