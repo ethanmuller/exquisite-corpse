c8e7f5c2ec6a71afc341751bbb3801c7e613cb39
Ethan Muller <hi@emu.media>
2023-08-07 07:37:01 -0400

    formatting

--- src/CallToAction.tsx
+++ src/CallToAction.tsx
@@ -12,14 +12,31 @@ function CopyPrompt(props) {
   const [copied, setCopied] = useState(false);
 
   return (
-    <div style={{fontSize: '0.75rem', lineHeight: 1.3, maxWidth: 280, margin: '0 auto', textAlign: 'left', textWrap: 'balance', position: 'relative'}}>
+    <div
+      style={{
+        fontSize: "0.75rem",
+        lineHeight: 1.3,
+        maxWidth: 280,
+        margin: "0 auto",
+        textAlign: "left",
+        textWrap: "balance",
+        position: "relative",
+      }}
+    >
       <div>Please send this URL to a friend to continue the drawing</div>
 
-      <div style={{display: 'flex', width: '100%', padding: '0.25rem 0 1rem'}}>
+      <div
+        style={{ display: "flex", width: "100%", padding: "0.25rem 0 1rem" }}
+      >
         <input
           readOnly
           type="text"
-          style={{padding: '0.5rem 0 0.5rem 0.5rem', flex: 1, borderTopRightRadius: 0, borderBottomRightRadius: 0}}
+          style={{
+            padding: "0.5rem 0 0.5rem 0.5rem",
+            flex: 1,
+            borderTopRightRadius: 0,
+            borderBottomRightRadius: 0,
+          }}
           value={`${import.meta.env.VITE_CLIENT_URL}${
             props.game.id
           }?part=${stateToPart(props.game.gameState)}`}
@@ -29,19 +46,42 @@ function CopyPrompt(props) {
             props.game.id
           }?part=${stateToPart(props.game.gameState)}`}
           onCopy={() => {
-            setCopied(true)
+            setCopied(true);
           }}
         >
-          <button className='btn__primary' style={{borderTopLeftRadius: 0, borderBottomLeftRadius: 0}}>Copy</button>
+          <button
+            className="btn__primary"
+            style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+          >
+            Copy
+          </button>
         </CopyToClipboard>
       </div>
-      {copied ? <span style={{ color: "green", fontFamily: 'Inter, sans-serif', position: 'absolute', textAlign: 'right', width: '100%', marginTop: '-0.75rem'}}> Copied to clipboard</span> : null}
+      {copied ? (
+        <span
+          style={{
+            color: "green",
+            fontFamily: "Inter, sans-serif",
+            position: "absolute",
+            textAlign: "right",
+            width: "100%",
+            marginTop: "-0.75rem",
+          }}
+        >
+          {" "}
+          Copied to clipboard
+        </span>
+      ) : null}
     </div>
   );
 }
 
 function DrawPrompt(props) {
-  return <div>Please draw <strong>the {props.part}</strong></div>
+  return (
+    <div>
+      Please draw <strong>the {props.part}</strong>
+    </div>
+  );
 }
 
 function LinkToFullCorpse(props) {
@@ -58,19 +98,19 @@ export function CallToAction(props) {
     DRAW: DrawPrompt,
     COPY: CopyPrompt,
     FINAL_LINK: LinkToFullCorpse,
-  }
+  };
 
   let view;
 
   if (props.game.gameState === 3) {
-    view = 'FINAL_LINK'
+    view = "FINAL_LINK";
   } else if (props.isThisPartOver) {
-    view = 'COPY'
+    view = "COPY";
   } else {
-    view = 'DRAW'
+    view = "DRAW";
   }
 
-  const CurrentView = views[view]
+  const CurrentView = views[view];
 
-  return <CurrentView {...props} />
+  return <CurrentView {...props} />;
 }
--- src/Controls.tsx
+++ src/Controls.tsx
@@ -3,20 +3,18 @@ import "react";
 function Buttons(props) {
   return (
     <div>
-      <span className="sp"><button onClick={props.onClear}>Clear</button></span>
-      <span className="sp"><button onClick={props.onDone} className='btn__primary'>All Done</button></span>
+      <span className="sp">
+        <button onClick={props.onClear}>Clear</button>
+      </span>
+      <span className="sp">
+        <button onClick={props.onDone} className="btn__primary">
+          All Done
+        </button>
+      </span>
     </div>
   );
 }
 
 export function Controls(props) {
-  return (
-    <>
-      {props.isThisPartOver ? (
-        <></>
-      ) : (
-        <Buttons {...props} />
-      )}
-    </>
-  );
+  return <>{props.isThisPartOver ? <></> : <Buttons {...props} />}</>;
 }
